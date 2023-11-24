e7efd0c94aacfedc5311f537459c83730f34c004
Elvis334 <138623471+Elvis334@users.noreply.github.com>
2023-11-24 09:56:27 -0600

    version inicial calculadora

---
+++ main.py
@@ -0,0 +1,19 @@
+#Calculadora
+
+#Elvis
+def suma():
+    pass
+
+
+def resta():
+    pass
+
+#--------------------------------------------------
+
+#Javier
+def division():
+    pass
+
+def multiplicacion():
+    pass
+
